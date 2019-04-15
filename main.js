937c478013cf0dbf296216c1e33cbd4ac95a95ca
Nina246 <49496766+Nina246@users.noreply.github.com>
2019-04-15 17:18:01 +0200

    Add files via upload
    JavaScript file zur Implementierung der Textbox

---
+++ main.js
@@ -0,0 +1,21 @@
+document.querySelector('html').onclick = function() {};
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('p');
+
+function setUserName(){
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Hello, ' + myName + '!';
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Hello, ' + storedName + '!';
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
